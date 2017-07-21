1a4e394aef519703028f9b8ada529c9d8daea79b
王迪 <wangdi@tranzvision.com.cn>
2017-07-21 18:09:27 +0800

    xxx

--- advids-webapp/src/main/java/Test.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by advids on 2017/5/31.
- */
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
---
+++ advids-webapp/src/main/java/util/StringUtil.java
@@ -0,0 +1,9 @@
+package util;
+
+/**
+ * Created by WangDi on 2017/7/21.
+ */
+public class StringUtil {
+
+
+}
